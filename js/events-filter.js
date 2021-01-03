0f906afc6191fc194134cd390d1f16be61e71d8d
Pavel <paveljurca@users.noreply.github.com>
2021-01-03 22:00:35 +0100

    v1, TODO: booking form

---
+++ js/events-filter.js
@@ -0,0 +1,51 @@
+/*
+Events filter
+v01
+*/
+    (function($) {
+  "use strict"; // Start of use strict
+
+    // INITIALIZE DATEPICKER PLUGIN
+    $('#eventsFilter').datepicker({
+    	format: "mm-yyyy",
+    	startView: "months", 
+    	minViewMode: "months",
+        clearBtn: true,
+        language: "de",
+        autoclose: true,
+	startDate: "06-2021", // start season in June
+        beforeShowMonth: function (date) {
+    	  var m = date.getMonth();
+	  // disable months: September, October, November and December
+          if ((m == 9) || (m == 10) || (m == 11 || (m == 12))) {
+            return false;
+          }
+        }
+    });
+
+
+    $('#eventsFilter').on('changeDate', function () {
+        var pickedDate = $('input').val();
+
+	if (pickedDate == "") {
+		// FILTER CLEARED
+        	$('.event-date').removeClass('hide');
+        	// show Most 2020 heading
+		$('#most2020').removeClass('hide');
+	} else {
+		// FILTER BY MONTH
+        	$('.event-date').addClass('hide');
+        	$(`.event-date[month="${pickedDate}"]`).removeClass('hide');
+        	// hide Most 2020 heading
+		$('#most2020').addClass('hide');
+	}
+    });
+
+    /*
+    $('#eventsFilter').on('clearDate', function () {
+   	alert("CLEARED");
+    });
+    */
+
+})(jQuery); // End of use strict
+
--- js/scripts.js
+++ js/scripts.js
@@ -45,7 +45,7 @@
   $(window).scroll(navbarCollapse);
 
   // Magnific popup calls
-  $('#portfolio').magnificPopup({
+  $('.container-fluid').magnificPopup({
     delegate: 'a',
     type: 'image',
     tLoading: 'Loading image #%curr%...',
